71e76c47b82caacea5dae8687ff0875f598637b9
Александр Мельников <AMelnikov@migcredit.ru>
2022-05-20 15:15:09 +0300

    Task_6 Работа над авторизацией

--- app/api_amount/settings.py
+++ app/api_amount/settings.py
@@ -1,17 +1,16 @@
 from pydantic import BaseSettings
-from functools import lru_cache
 
 
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
 
+    database_url: str
+
     jwt_secret: str
     jwt_algorithm: str = 'HS256'
-    jwt_time: int = 3600
+    jwt_expiration: int = 3600
 
 
-@lru_cache()
-def settings():
-    return Settings(_env_file='.env',
+settings = Settings(_env_file='.env',
                     _env_file_encoding='utf-8')
